bd8748c4cac4d4c75a4ae5a606e9c9808ff39605
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2018-07-30 22:29:17 -0500

    Add jinja2 template env
    Access the jinja2 template engine to have more fine tuned control over whitespace in the rendered template
    
    Start updating styles to get proper layout

--- generate_report.py
+++ generate_report.py
@@ -1,7 +1,6 @@
 import sqlite3 as sq3
-# import settings
 import os
-from jinja2 import Template
+from jinja2 import Template, Environment, FileSystemLoader
 from datetime import datetime
 import json
 
@@ -14,12 +13,12 @@ class Report(object):
             self.layout = json.loads(f.read())
         self.paths = self.layout['paths']
         self.cursor = sq3.connect(self.paths['database']).cursor()
-        self.categories = self.get_categries()
-        # self.title_map = self.layout['titles']
-        # print(self.title_map)
+        self.categories = self.__get_categries()
+        self.env = Environment(trim_blocks=True, lstrip_blocks=True,
+                  loader=FileSystemLoader(self.paths['template_dir']))
 
     @staticmethod
-    def read_file(filename):
+    def __read_file(filename):
         """return the sql for the given file"""
         with open(filename, 'r') as f:
             return f.read()
@@ -27,16 +26,16 @@ class Report(object):
     def get_views(self):
         """return a list of view names from database"""
         filename = os.path.join(self.paths['SQL'], 'get_views.sql')
-        data = self.get_data(filename)
+        data = self.__get_data(filename)
         views = [view[0] for view in data]
         return views
 
-    def get_data(self, filename):
+    def __get_data(self, filename):
         """return cursor object of data"""
-        sql = self.read_file(filename)
+        sql = self.__read_file(filename)
         return self.cursor.execute(sql)
 
-    def get_all_data(self):
+    def __get_all_data(self):
         """return a dictionary containing all the data for all views"""
         views = self.get_views()
         data = {}
@@ -47,7 +46,7 @@ class Report(object):
             data.setdefault(view, rows)
         return data
 
-    def get_columns(self, table_name):
+    def __get_columns(self, table_name):
         """return a list of columns for the given table"""
         sql = '''PRAGMA table_info("{}")'''
         sql = sql.format(table_name)
@@ -56,7 +55,7 @@ class Report(object):
         cols = [col[1] for col in c]
         return cols
 
-    def get_title(self, view_names):
+    def __get_title(self, view_names):
         """return the name/title to be used as the page title"""
         map_names = self.layout['titles']
         titles = []
@@ -68,7 +67,7 @@ class Report(object):
 
         return titles
 
-    def get_categries(self):
+    def __get_categries(self):
         """
         Given the category name and list of view names, return
         a dictionary with category name and list of relative paths to
@@ -80,7 +79,7 @@ class Report(object):
         report = os.path.abspath(report)
         for key in cat_list:
             paths = []
-            titles = self.get_title(cat_list[key])
+            titles = self.__get_title(cat_list[key])
             for link in cat_list[key]:
                 path = os.path.join(report, link + '.html')
                 paths.append(path)
@@ -90,69 +89,51 @@ class Report(object):
         return categories
     def render_report(self, view_name):
         """render an output report"""
-        template = self.paths['template']
+
+        # Set up basic constants for this report
+        update = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
         report_dir = self.paths['reports']
         css_style = self.paths['css_styles']
         js = self.paths['javascript']
-
-        temp = self.read_file(template)
-        T = Template(temp)
-        update = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-
-        data = self.get_all_data()
-        rows = [row for row in data[view_name]]
-
-        headers = self.get_columns(view_name)
-        # print(headers)
+        headers = self.__get_columns(view_name)
         caption = self.layout['captions'][view_name]
-        title = self.get_title(view_name)
+        title = self.__get_title(view_name)
         description = self.layout['descriptions'][view_name]
         categories = self.categories
-        html = T.render(title=title,
-                        description=description,
-                        categories=categories,
-                        updated=update,
-                        caption=caption,
-                        css_style=css_style,
-                        js=js,
-                        headers=headers,
-                        rows=rows)
+
+
+        # Query database for all rows for view given by input
+        data = self.__get_all_data()
+        rows = [row for row in data[view_name]]
+
+        # Get the template for reports and render
+        temp = self.env.get_template(self.paths['template'])
+        html = temp.render(title=title,
+                           description=description,
+                           categories=categories,
+                           updated=update,
+                           caption=caption,
+                           css_style=css_style,
+                           js=js,
+                           headers=headers,
+                           rows=rows)
+
+        # Write rendered template to file in reports directory
         filename = '{}.html'.format(view_name)
         with open(os.path.join(report_dir, filename), 'w') as f:
             f.write(html)
 
     def render_all(self):
-        """render all templates"""
+        """render report for all views in database"""
         views = self.get_views()
         for view in views:
             self.render_report(view)
 
 
 def main():
-    # path = settings.DATABASE_PATH
     path = os.path.join('reports', 'templates', 'layout.json')
     R = Report(path)
     R.render_all()
-    # R.get_categries()
-    # v = R.get_views()
-    # print(v)
-    # t = R.get_title(v)
-    # print(t)
-    # t = R.get_title('DriveModelOptions')
-    # print(t)
-    # c = R.get_categries()
-    # print(c)
-    # h = R.get_columns('ListChains')
-    # R.render_report('ListChains')
-    # data = R.get_all_data()
-    # R.render_report()
-
-    # for key in data:
-    #     print(key)
-    #     for row in data[key]:
-    #         print('\ttest1{}'.format(row))
-
 
 if __name__ == '__main__':
-
     main()
