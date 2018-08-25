0bf4c56c1d6e3a4cb431be73b9bb5889445c302b
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2018-08-24 21:02:51 -0500

    Update setup.py to  install depenancies
    Add a simple CLI and install dependacies with pip.

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open('README.md', 'r') as f:
     long_description = f.read()
-description = 'Script to generate reports from views in sqlite3 database'
+description = 'Generate html reports of views in a sqlite3 database'
 keywords = 'report sqlite sqlite3 html'
 
-setuptools.setup(
+setup(
     name='dbreport',
     version='0.1dev',
     description=description,
     long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Joseph Contreras',
     author_email='26684136+JosephJContreras@users.noreply.github.com',
-    url='',
-    packages=setuptools.find_packages(),
+    url='https://github.com/JosephJContreras/dbreport.git',
+    packages=find_packages(),
+    classifiers=['Development Status::3 - Alpha'],
     keywords=keywords,
+    install_requires=['jinja2'],
+    entry_points={'console_scripts': ['report=dbreport:cli']},
     license='MIT')
