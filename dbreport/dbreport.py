b79d7608047c9c1e33f494fa98623cf4f89eb281
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2018-09-01 23:17:17 -0500

    Updates to improve code flow and data retrieval
    Changes to multiple functions to improve how the program flows through
    the class to avoid retrieving unused data and duplication.
    
    Rename several functions to more accurately describe usage, such as
    __set_default_settings, __add_misc_category, and
    __get_categories (errant)

--- dbreport/dbreport.py
+++ dbreport/dbreport.py
@@ -15,20 +15,107 @@ class Report(object):
         self.layout = self.__set_default_settings(user_layout)
         self.paths = self.layout['paths']
         self.cursor = sq3.connect(self.paths['database']).cursor()
-        self.categories = self.__get_categries()
-        self.env = Environment(trim_blocks=True, lstrip_blocks=True,
-                    loader=FileSystemLoader(self.paths['template_dir']))
+        self.categories = self.__get_categories()
+        self.env = Environment(
+            trim_blocks=True, lstrip_blocks=True,
+            loader=FileSystemLoader(self.paths['template_dir']))
 
-    @staticmethod
-    def __read_file(filename):
-        """return the sql for the given file"""
-        with open(filename, 'r') as f:
-            return f.read()
+    def __set_default_settings(self, user_layout, default_layout=None):
+        """
+        Set the default settings that are not set in the user
+        layout file
+        """
+
+        def set_default_paths():
+            """
+            Get default paths. If the path is in the specified layout
+            file, that path should be used, otherwise fall back and use
+            the default
+            """
+            # Read the default layout file provided by the package
+            package_path = os.path.dirname(__file__)
+            path = os.path.join(package_path, 'templates', 'layout.json')
+            with open(path, 'r') as f:
+                def_layout = json.loads(f.read())
+
+            # expand paths in default layout to be absolute
+            template_dir = os.path.join(package_path,
+                                        def_layout['paths']['template_dir'][0])
+            def_layout['paths']['template_dir'] = template_dir
+
+            # iterate over path lists and update them be an absolute path
+            # pointing to the package defaults
+            for p in ['css_styles', 'javascript', 'sql']:
+                paths = []
+                for path in def_layout['paths'][p]:
+                    paths.append(os.path.join(template_dir, path))
+                def_layout['paths'][p] = paths
+            return def_layout
+
+        if default_layout is None:
+            # Set the paths to the layout file distributed
+            # with the package
+            default_layout = set_default_paths()
+        # Iterate over keys and set to default if not specified in user
+        # layout file
+        for key in default_layout:
+            user_layout.setdefault(key, default_layout[key])
+            if isinstance(default_layout[key], dict):
+                user_layout[key] = self.__set_default_settings(
+                    user_layout[key], default_layout[key])
+
+        return user_layout
+
+    def __add_misc_category(self, categories, views):
+        """
+        Return a dictionary with the categories defined in the layout
+        file, as well as an additional 'Misc' category that contains
+        any view, not listed in the ignore_views list, that is not
+        specified in another category. This will ensure that there will
+        be a convenient way to access all reports from the navigation
+        bar in each report.
+        """
+
+        # iterate over categories and remove any view name that is
+        # listed as in the categories
+        for category in categories:
+            for view in categories[category]:
+                if view in views:
+                    views.remove(view)
+        categories.setdefault('Misc', views)
+        return categories
+
+    def __get_categories(self):
+        """
+        Given the category name and list of view names, return
+        a dictionary with category name and list of relative paths to
+        the report for that view
+        """
+
+        categories = self.layout['categories']
+        cat_list = self.__add_misc_category(
+            categories=self.layout['categories'],
+            views=self.__get_views())
+
+        categories = {}
+        for key in cat_list:
+            paths = []
+            titles = self.__get_title(cat_list[key])
+            for link in cat_list[key]:
+                # Note all the reports are all in the same folder
+                path = os.path.join('.', link + '.html')
+                paths.append(path)
+                val = (titles, paths)
+            categories.setdefault(key, val)
+
+        return categories
 
     def __get_views(self):
         """return a list of view names from database"""
         filename = os.path.join(self.paths['sql'][0], 'get_views.sql')
-        data = self.__get_data(filename)
+        with open(filename, 'r') as f:
+            sql = f.read()
+        data = self.cursor.execute(sql)
         views = [view[0] for view in data]
 
         # Remove any views that are in the ignore list in the layout
@@ -39,20 +126,23 @@ class Report(object):
                 views.remove(ignore_view)
         return views
 
-    def __get_data(self, filename):
-        """return cursor object of data"""
-        sql = self.__read_file(filename)
-        return self.cursor.execute(sql)
+    def __get_data(self, views=None):
+        """get the data for the view(s) given"""
 
-    def __get_all_data(self):
-        """return a dictionary containing all the data for all views"""
-        views = self.__get_views()
+        if views is None:
+            # views are None, set to use all views
+            views = self.__get_views()
+        elif not isinstance(views, list):
+            # make the view a list
+            views = [views]
+
         data = {}
         for view in views:
-            sql = '''SELECT * FROM {}'''.format(view)
+            sql = 'SELECT * FROM {}'.format(view)
             results = self.cursor.execute(sql)
             rows = [result for result in results]
             data.setdefault(view, rows)
+
         return data
 
     def __get_columns(self, table_name):
@@ -67,53 +157,15 @@ class Report(object):
         """return the name/title to be used as the page title"""
         map_names = self.layout['titles']
         titles = []
-        if type(view_names) is list:
+        if isinstance(view_names, list):
             for view in view_names:
                 titles.append(map_names.get(view, view))
         else:
             titles = map_names.get(view_names, view_names)
         return titles
 
-    def __get_misc(self):
-        """
-        Update the categories to include a Misc category that will
-        include all views in the database that is not specified in
-        another category. This will ensure that there will be a
-        convenient way to access all reports from the navigation bar
-        in each report.
-        """
-        categories = self.layout['categories']
-        views = self.__get_views()
-        for category in categories:
-            for view in categories[category]:
-                if view in views:
-                    views.remove(view)
-        categories.setdefault('Misc', views)
-        return categories
-
-    def __get_categries(self):
-        """
-        Given the category name and list of view names, return
-        a dictionary with category name and list of relative paths to
-        the report for that view
-        """
-        cat_list = self.__get_misc()
-        categories = {}
-        for key in cat_list:
-            paths = []
-            titles = self.__get_title(cat_list[key])
-            for link in cat_list[key]:
-                # Note all the reports are all in the same folder
-                path = os.path.join('.', link + '.html')
-                paths.append(path)
-                val = [titles, paths]
-            categories.setdefault(key, val)
-
-        return categories
-
-    def __render_report(self, view_name, parse=True):
+    def __render_report(self, view_name, data, parse=True):
         """render an output report"""
-
         # Set up basic constants for this report
         update = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
         report_dir = self.paths['report_dir']
@@ -126,7 +178,7 @@ class Report(object):
         categories = self.categories
 
         # Query database for all rows for view given by input
-        data = self.__get_all_data()
+        # data = self.__get_all_data()
         if parse:
             # call the parse function that may be overloaded
             data = self.parse(data)
@@ -169,7 +221,8 @@ class Report(object):
             views = self.__get_views()
 
         for view in views:
-            self.__render_report(view, parse)
+            data = self.__get_data(view)
+            self.__render_report(view, data, parse)
 
         return len(views)
 
@@ -181,46 +234,3 @@ class Report(object):
         and a tuple for each row.
         """
         return data
-
-    def __set_default_paths(self):
-        """get default paths. If the path is in the specified layout
-        file, that path should be used, otherwise fall back and use the
-        default"""
-
-        # Read the default layout file provided by the package
-        package_path = os.path.dirname(__file__)
-        path = os.path.join(package_path, 'templates', 'layout.json')
-        with open(path, 'r') as f:
-            def_layout = json.loads(f.read())
-
-        # expand paths in default layout to be absolute
-        template_dir = os.path.join(package_path,
-                       def_layout['paths']['template_dir'][0])
-        def_layout['paths']['template_dir'] = template_dir
-
-        # iterate over path lists and update them be an absolute path
-        # pointing to the package defaults
-        for p in ['css_styles', 'javascript', 'sql']:
-            paths = []
-            for path in def_layout['paths'][p]:
-                paths.append(os.path.join(template_dir, path))
-            def_layout['paths'][p] = paths
-        return def_layout
-
-    def __set_default_settings(self, user_layout, default_layout=None):
-        """set the default settings that are not set in the user
-        layout file"""
-
-        if default_layout is None:
-            # Set the paths to the layout file distributed
-            # with the package
-            default_layout = self.__set_default_paths()
-        # Iterate over keys and set to default if not specified in user
-        # layout file
-        for key in default_layout:
-            user_layout.setdefault(key, default_layout[key])
-            if isinstance(default_layout[key], dict):
-                user_layout[key] = self.__set_default_settings(
-                    user_layout[key], default_layout[key])
-
-        return user_layout
