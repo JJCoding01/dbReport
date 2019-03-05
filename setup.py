e5c2129a32d33ee7d740e4a2461f2646322818dc
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2019-03-04 22:57:30 -0600

    updated version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ paths = ['templates/' + p for p in paths]
 
 setup(
     name='dbreport',
-    version='0.2dev',
+    version='0.3.0dev',
     description=description,
     long_description=long_description,
     long_description_content_type='text/markdown',
