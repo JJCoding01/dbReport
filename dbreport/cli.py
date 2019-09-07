27bfffcaa7db348ffbb3afb31aa4581edfbed509
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2019-09-07 09:54:56 -0500

    Reformat project with black

--- dbreport/cli.py
+++ dbreport/cli.py
@@ -4,18 +4,18 @@ import argparse
 
 def cli():
     """define a CLI for dbreport"""
-    PARSER = argparse.ArgumentParser(description='generate reports from db views')
+    PARSER = argparse.ArgumentParser(
+        description="generate reports from db views"
+    )
 
-    PARSER.add_argument('path',
-                        nargs='?',
-                        help='path to layout file',
-                        default=None,
-                        type=str)
+    PARSER.add_argument(
+        "path", nargs="?", help="path to layout file", default=None, type=str
+    )
 
     ARGS = PARSER.parse_args()
     r = Report(ARGS.path)
     r.render_all()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
