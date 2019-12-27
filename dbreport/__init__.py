f753a51303ee9308d32116c90752998faf3b1268
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2019-12-27 16:23:30 -0600

    Add various properties to init

--- dbreport/__init__.py
+++ dbreport/__init__.py
@@ -1,2 +1,8 @@
+"""Generator for SQLite database"""
 from .dbreport import Report
 from .cli import cli
+
+__version__ = "0.3.2dev"
+__author__ = "Joseph Contreras Jr."
+__license__ = "MIT"
+__copyright__ = "Copyright 2018 Joseph Contreras Jr."
