34ae891dbeae96b4ca47eff723596687b089546d
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2019-12-29 21:38:30 -0600

    Remove experimental mocks in tests

--- tests/dbreport/test_dbreport.py
+++ tests/dbreport/test_dbreport.py
@@ -1,24 +1,7 @@
 
 import pytest
-# from pytest_mock import mock
-import mock
-from pytest_mock import mocker
 from dbreport.dbreport import Report
 
-@mock.path('dbreport.dbreport.Report.parse')
-def test_parse(mock_parse):
-
-
 def test_init():
-
-    with pytest.raises(TypeError):
+    with pytest.raises(ValueError):
         Report('invalid/path/that/is/not/a/json/file')
-
-
-
-def test_something():
-    pytest.fail()
-
-
-def test_parse():
-    pass
