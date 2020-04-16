bc2513814b230521b8d3a097a61b946ef76100bd
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2020-04-15 21:28:28 -0500

    Bump version to 0.3.3a2

--- dbreport/__init__.py
+++ dbreport/__init__.py
@@ -1,7 +1,7 @@
 """Generator for SQLite database"""
 from .dbreport import Report
 
-__version__ = "0.3.3a1"
+__version__ = "0.3.3a2"
 __author__ = "Joseph Contreras Jr."
 __license__ = "MIT"
 __copyright__ = "Copyright 2018 Joseph Contreras Jr."
