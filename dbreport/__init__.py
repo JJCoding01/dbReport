4842ab932ce7451ad788885c9c3ce17515845da7
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2026-05-07 22:11:05 -0500

    Remove extra meta-data from __init__.py

--- dbreport/__init__.py
+++ dbreport/__init__.py
@@ -8,6 +8,3 @@ from .layout import Paths
 from .report import Report
 
 __version__ = "0.3.3a2"
-__author__ = "Joseph Contreras Jr."
-__license__ = "MIT"
-__copyright__ = "Copyright 2018 Joseph Contreras Jr."
